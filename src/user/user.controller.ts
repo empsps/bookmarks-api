23ef12f15689a2ec1c40ac4ca73704bb9e3841aa
empsps <sophialealbarros@gmail.com>
2022-12-06 22:14:33 -0300

    feat(users): add feature to edit user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, UseGuards } from '@nestjs/common';
 import { User } from '@prisma/client';
 import { GetUser } from '../auth/decorator';
 import { JwtGuard } from '../auth/guard';
+import { EditUserDTO } from './dto';
+import { UserService } from './user.service';
 
 @UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
+  constructor(private service: UserService) {}
+
   @Get('me')
   getLoggedInUser(@GetUser() user: User) {
     return user;
+  }
+
+  @Patch()
+  editUser(@GetUser('id') userId: string, @Body() dto: EditUserDTO) {
+    return this.service.editUser(userId, dto);
   }
 }
